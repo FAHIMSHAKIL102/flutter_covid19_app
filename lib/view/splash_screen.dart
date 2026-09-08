9cb0241d213c5a33c3a1cdc9b3296c51efe626be
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-09-08 12:08:13 +0600

    feat: splash screen complete

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math' as math;
+
 class SplashScreen extends StatefulWidget {
-  const new({super.key});
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -9,6 +11,17 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 3),
+  )..repeat();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -21,7 +34,30 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           mainAxisAlignment: .center,
           crossAxisAlignment: .center,
-          children: [],
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    child: Image(image: AssetImage('assets/images/virus.png')),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: MediaQuery.heightOf(context) * .08),
+            Align(
+              alignment: .center,
+              child: Text(
+                'Covid-19 \nTracker App',
+                textAlign: .center,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ),
       ),
     );
